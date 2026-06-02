f3d5abf86d502b9f63f22679b519a38b400391b7
kellyncodes <kellyncodes@gmail.com>
2026-06-02 13:42:25 +0100

    chore

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ print(df.describe())
 print(df.info())
 print(df.columns)
 
+
 """'customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents',
        'tenure', 'PhoneService', 'MultipleLines', 'InternetService',
        'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
@@ -32,6 +33,7 @@ df['TechSupport'] = df['TechSupport'].map({'Yes': 1, 'No': 0})
 df['StreamingTV'] = df['StreamingTV'].map({'Yes': 1, 'No': 0})
 df['StreamingMovies'] = df['StreamingMovies'].map({'Yes': 1, 'No': 0})
 df['PaperlessBilling'] = df['PaperlessBilling'].map({'Yes': 1, 'No': 0})
+df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
 
 
 
@@ -62,3 +64,38 @@ plt.show()"""
 #plt.show()
 
 #print(df.iloc[:,4:])
+print(df['Churn'])
+
+print(df.head(10))
+print(df.describe())
+print(df.info())
+print(df.columns)
+
+#print(df['customerID'])
+df = df.drop('customerID', axis=1) # the customer ID is usless here because it doesn't determine the diecision or affect the decision making of a customer
+#print(df['customerID'])
+#print(df.columns)
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+print(df['TotalCharges'])
+
+print(df.isnull().sum())
+
+# The features with customer service equals "Yes" has Nan across the columns for each row it appears 
+
+internet_related_cols = [
+    'InternetService',
+    'OnlineSecurity',
+    'OnlineBackup',
+    'DeviceProtection',
+    'TechSupport',
+    'StreamingTV',
+    'StreamingMovies'
+]
+
+for col in internet_related_cols:
+    df[col] = df[col].fillna('No internet service')
+
+print(df.isnull().sum())
+
+sns.pairplot(df)
+plt.show()
